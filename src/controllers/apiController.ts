cc2d57396f1e955bc9bf15d5f3e3947055ba10c8
LeonardoBuscarioloGC <leonardo.buscariolo@grancoffee.com.br>
2022-03-19 13:14:05 -0300

    versao final

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -4,6 +4,10 @@ import { Op, Sequelize } from "sequelize";
 import { off } from "process";
 import sharp from "sharp";
 import { unlink } from "fs/promises";
+import JWT from "jsonwebtoken";
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 
 export const ping = (req: Request, res: Response) => {
@@ -22,8 +26,14 @@ export const register = async (req: Request, res: Response) => {
             }
         });
         if(created) {
+            const token = JWT.sign(
+                {id: user.id, email: user.email},
+                process.env.JWT_SECRET_KEY as string,
+                {expiresIn: '2h'}
+            );
+
             res.status(201);
-            res.json({user})
+            res.json({user, token})
         } else {
             res.json({error: 'E-mail já cadastrado'})
         }
@@ -31,7 +41,31 @@ export const register = async (req: Request, res: Response) => {
 }
 
 export const login = async (req: Request, res: Response) => {
-    res.json({})
+    if(req.body.email && req.body.password) {
+        let {email, password} = req.body;
+
+        let user = await User.findOne({
+            where: {
+                email: email,
+                password: password
+            }
+        });
+
+        if (user) {
+            const token = JWT.sign(
+                {id: user.id, email: user.email},
+                process.env.JWT_SECRET_KEY as string,
+                {expiresIn: '2h'}
+            );
+
+            res.json({status: true, token})
+        } else {
+            res.json({status: false})
+        }
+
+    } else {
+        res.json({error: 'E-mail ou senha não inseridos'});
+    }
 }
 
 export const list = async (req: Request, res: Response) => {
